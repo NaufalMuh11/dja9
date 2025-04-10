7cd3eeee5e6a6245494a2590e8264338db501ef8
Muh11 <naufalaswinana@gmail.com>
2025-04-10 22:37:39 +0700

    feat: create model for get top modul

--- application/models/MyTask_model.php
+++ application/models/MyTask_model.php
@@ -30,4 +30,18 @@ class MyTask_model extends CI_Model
 		$this->db->limit(3);
 		return $this->db->get()->result();
 	}
+
+	public function get_top_module_distribution($year, $month) 
+	{
+		$this->db->select('t_mytask_link.keterangan, COUNT(t_mytask_log.id) as access_count');
+		$this->db->from('t_mytask_log');
+		$this->db->join('t_mytask_link', 't_mytask_log.buttonid = t_mytask_link.buttonid', 'left');
+		$this->db->where('YEAR(t_mytask_log.datetime)', $year);
+		$this->db->where('MONTH(t_mytask_log.datetime)', $month);
+		$this->db->group_by('t_mytask_link.buttonid, t_mytask_link.keterangan');
+		$this->db->order_by('access_count', 'DESC');
+		$this->db->limit(5);
+		return $this->db->get()->result();
+}
+
 }
